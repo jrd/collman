ddbc67cb2916f9a098397ea886b23ec0353761af
Cyrille Pontvieux <jrd@animeka.com>
2014-05-27 18:40:41 +0200

    Some tests

--- server/index.js
+++ server/index.js
@@ -4,4 +4,18 @@ var app = express();
 app.get('/', function(req, res) {
   res.send('CollMan');
 });
+app.get('/test', function(req, res) {
+  res.send('ça marche');
+});
+app.get(/^\/bonjour\/(.+)$/, function(req, res) {
+  res.send('Bonjour ' + req.params[0] + ' !');
+});
+app.get('/bonsoir/:name', function(req, res) {
+  res.send('Bonsoir ' + req.params['name'] + ' !');
+});
+app.use(function(req, res, next) {
+  console.log('%s %s', req.method, req.url);
+  next();
+});
+app.use('/contenu', express.static(__dirname));
 app.listen(3000);
